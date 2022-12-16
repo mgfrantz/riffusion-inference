65cb37b16578ea21e5b90f8c7a03d7d49c11e3f8
Michael Frantz <michaelgfrantz@gmail.com>
2022-12-16 17:08:03 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+with open('requirements.txt', 'r') as f:
+    packages = [i.strip() for i in f.readlines()]
+
+setup(name='riffusion',
+      version='0.1',
+      description='Python Distribution Utilities',
+      author='Greg Ward',
+      author_email='gward@python.net',
+      url='https://www.python.org/sigs/distutils-sig/',
+      packages=['riffusion'],
+      requires=packages
+     )
