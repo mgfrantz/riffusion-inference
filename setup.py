f94b0b57046c87f3fdf30950e3ade69da0217b17
Michael Frantz <michaelgfrantz@gmail.com>
2022-12-26 20:54:40 +0000

    Edited author

--- setup.py
+++ setup.py
@@ -5,10 +5,6 @@ with open('requirements.txt', 'r') as f:
 
 setup(name='riffusion',
       version='0.1',
-      description='Python Distribution Utilities',
-      author='Greg Ward',
-      author_email='gward@python.net',
-      url='https://www.python.org/sigs/distutils-sig/',
       packages=['riffusion'],
       install_requires=packages
      )
